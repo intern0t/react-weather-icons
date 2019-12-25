680d7d855b63035ff33ad1b1a8c901917de58fb8
Prashant Shrestha <intern0t@users.noreply.github.com>
2019-12-24 22:50:20 -0500

    Updated jest's test cases for basic checks.

--- src/test.js
+++ src/test.js
@@ -1,7 +1,25 @@
-import ExampleComponent from './'
+import { DirectionUp, NightCloudyGusts, NightCloudyHigh } from "./index";
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
-  })
-})
+describe("Testing couple icon(s).", () => {
+	it("has SVG component.", () => {
+		expect(DirectionUp.toString()).toContain("svg");
+	});
+
+	it("has no style tags.", () => {
+		expect(DirectionUp.toString()).not.toContain("<style");
+		expect(NightCloudyGusts.toString()).not.toContain("<style");
+		expect(NightCloudyHigh.toString()).not.toContain("<style");
+	});
+
+	it("has default size & color properties.", () => {
+		expect(DirectionUp.toString()).toContain(
+			"_objectWithoutProperties(props, ['color', 'size']);"
+		);
+		expect(NightCloudyGusts.toString()).toContain(
+			"_objectWithoutProperties(props, ['color', 'size']);"
+		);
+		expect(NightCloudyHigh.toString()).toContain(
+			"_objectWithoutProperties(props, ['color', 'size']);"
+		);
+	});
+});
